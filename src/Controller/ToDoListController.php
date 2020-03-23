6233360a5115ce4a03bc867bc87d4882284c05d2
HarrisonPro <frnharrison@gmail.com>
2020-03-23 17:44:54 +0100

    Display list of all task

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -14,9 +14,9 @@ class ToDoListController extends AbstractController
      */
     public function index()
     {
-        return $this->render('to_do_list/index.html.twig', [
-            'controller_name' => 'ToDoListController',
-        ]);
+        $tasks = $this->getDoctrine()->getRepository(Task::class)->findBy([],
+        ['id'=>'DESC']);
+        return $this->render('to_do_list/index.html.twig', ['tasks'=>$tasks]);
     }
 
     /**
